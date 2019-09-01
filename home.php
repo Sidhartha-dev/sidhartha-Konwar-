b553a8fa5c5c373db0b9a23f6167a6a0756e06d8
Siddev <Sidhartha@gmail.com>
2019-09-01 22:51:30 +0530

    home.php and jiosign.html and laki.html and log.css and registration.php and validation.php added

---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <title>Home page</title>
+     <style>
+     body{
+       background-color: black;
+     }
+     h1{
+       color: #fff;
+     }
+     </style>
+   </head>
+   <body>
+    <h1> Hi Welcome <br>
+    <a href="laki.html">click here </a></h1>
+   </body>
+ </html>
